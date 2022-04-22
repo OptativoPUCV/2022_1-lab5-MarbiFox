33ca0c74d6c6512738c61638d9decd64c722743f
MarbiFox <>
2022-04-22 18:33:55 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -95,7 +95,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
         }
         else {
             node = node->right;
-            node->right->parent = node->right;
+            node->right->parent = node->parent;
             
             
         }
